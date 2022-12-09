29f106eb1fad6d8bf124be60aa02cf773e671339
Gina Rodolfo <110985809+STINGRAY008@users.noreply.github.com>
2022-12-09 19:51:25 +0800

    add axios to package jason

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -1,19 +1,23 @@
 import React, {useState} from 'react';
 import "./App.css";
 import Axios from 'axios'
+import Recipe from './components/Recipe'
 
 const App = () => {
   const [query, setQuery] = useState("");
+  const[recipes, setRecipes] = useState ([]);
 
 
-  const APP_ID = "481932d2"
-  const APP_KEY = "a793dbbe28d4ea0a39bbb619692069fa"
-  const url = 'https://api.edamam.com/search?q={query}&app_id=${APP_ID}&app_key=${APP_KEY}';
+  const APP_ID = "481932d2";
+  const APP_KEY = "a793dbbe28d4ea0a39bbb619692069fa";
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   const getData = async () => {
     const result = await Axios.get(url);
+    setRecipes(result.data.hits)
 
-    console.log(result)
+    console.log(result);
+    setQuery('');
   };
 
   const onChange = e => {
@@ -29,9 +33,20 @@ const App = () => {
     <div className="App">
       <h1>Food Searching Menu</h1>
       <form className="search-form" onSubmit= {onSubmit}>
-        <input type="text" placeholder="Search Food" autoComplete="off" onChange= {onChange} />
+        <input 
+          type="text" 
+          placeholder="Search Food" 
+          autoComplete="off" 
+          onChange= {onChange}
+          value={query} 
+          />
         <input type="submit" value="search"/>
       </form>
+      <div className="recipes">
+        {recipes !== [] && recipes.map(recipe => 
+        <Recipe recipe={recipe}/>)}
+
+      </div>
     </div>
   );
 };
